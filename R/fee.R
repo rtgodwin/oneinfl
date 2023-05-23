0885a899649ed37fc24d0466163bb62653183085
rtgodwin <42013175+rtgodwin@users.noreply.github.com>
2023-05-22 21:53:25 -0500

    Update fee.R

--- R/fee.R
+++ R/fee.R
@@ -7,7 +7,6 @@ fee <- function(model, data, at="AE") {
   cleandata <- makeXZy(formula, data)
   X <- cleandata$X
   Z <- cleandata$Z
-
   if (is.list(at)) {
     q$where <- "First exposure effect evaluated at: "
     for(i in 1:(length(at) - 1)) {
@@ -27,13 +26,11 @@ fee <- function(model, data, at="AE") {
   } else {
     stop("'at' must be 'AE' (average effect), 'EM' (effect at means), or a list of representative cases in which to evaluate the effect")
   }
-
   if (model$dist == "Poisson") {
     q$fee <- mean(fee_pois(b, g, X, Z))
     J <- as.matrix(colMeans(attr(numericDeriv(quote(fee_pois(b, g, X, Z)), c("b", "g")), "gradient")))
     q$sefee <- sqrt(t(J) %*% model$vc %*% J)
     q$treatment_visits <- sum(fee_pois(b, g, X, Z))
-
   } else if (model$dist == "negbin") {
     q$fee <- mean(fee_negbin(b, g, a, X, Z))
     J <- as.matrix(colMeans(attr(numericDeriv(quote(fee_negbin(b, g, a, X, Z)), c("b", "g", "a")), "gradient")))
