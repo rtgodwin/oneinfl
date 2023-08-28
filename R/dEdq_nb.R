8ccd7ee5cb96af274b6d87d1c6fa6767ebbf459f
rtgodwin <42013175+rtgodwin@users.noreply.github.com>
2023-08-28 16:07:27 -0500

    Update dEdq_nb.R

--- R/dEdq_nb.R
+++ R/dEdq_nb.R
@@ -2,6 +2,7 @@ dEdq_nb <- function(b, g, a, X, Z, dummies) {
   l <- exp(X %*% b)
   t <- exp(-Z %*% g)
   th <- l / a
+  P1 <- a * ((1 / (1 + th)) ^ a) * th / (1 + th - (1 + th) ^ (1 - a))
   L <- -(((a / (a + li)) ^ (-a)) * (1 / l) * (1 + l/a - (1 + l/a) ^ (1-a)) - 1) ^ (-1)
   w <- L + (1 - L) / (1 + t)
   
