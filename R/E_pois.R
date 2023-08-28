45f11974a3acc963134a5037ced343081c350831
rtgodwin <42013175+rtgodwin@users.noreply.github.com>
2023-08-28 14:54:10 -0500

    Update E_pois.R

--- R/E_pois.R
+++ R/E_pois.R
@@ -1,4 +1,4 @@
-fee_pois <- function(b, g, X, Z) {
+E_pois <- function(b, g, X, Z) {
   l <- exp(X %*% b)
   t <- exp(-Z %*% g)
   w <- -l * (exp(l) - l - 1) ^ -1 + (1 + l * (exp(l) - l - 1) ^ -1) * (1 + t) ^ -1
