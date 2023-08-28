dccafbfec72a1ace26b8a8673184a6a77365e3e2
rtgodwin <42013175+rtgodwin@users.noreply.github.com>
2023-08-28 15:09:54 -0500

    Update E_negbin.R

--- R/E_negbin.R
+++ R/E_negbin.R
@@ -1,9 +1,9 @@
-fee_negbin <- function(b, g, a, X, Z) {
+E_negbin <- function(b, g, a, X, Z) {
   l <- exp(X %*% b)
   t <- exp(-Z %*% g)
   th <- l / a
   P1 <- a * ((1 / (1 + th)) ^ a) * th / (1 + th - (1 + th) ^ (1 - a))
   L <- -P1 / (1 - P1)
   w <- L + (1 - L) / (1 + t)
-  w * (1 - l * (1 - (1 + th) ^ -a) ^ -1)
+  w + (1 - w) * (l / (1 - (1 + th) ^ (-a)))
 }
