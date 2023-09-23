238f72d9762e4358ba0a3d094a6e3743a9467eb9
rtgodwin <42013175+rtgodwin@users.noreply.github.com>
2023-09-22 21:45:07 -0500

    Update oneLRT.R

--- R/oneLRT.R
+++ R/oneLRT.R
@@ -1,6 +1,6 @@
 oneLRT <- function(mod0, mod1) {
-  k0 <- nrow(mod1$vc)
-  k1 <- nrow(mod0$vc)
+  k0 <- nrow(mod0$vc)
+  k1 <- nrow(mod1$vc)
   if (k1 > k0) {
     LRTstat <- -2 * (log(mod0$logl) - log(mod1$logl))
     pval <- 1 - pchisq(LRTstat, (k1 - k0))
