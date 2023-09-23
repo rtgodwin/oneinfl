5c6e2a19c14849da84bac31fec38c503a70f50fe
rtgodwin <42013175+rtgodwin@users.noreply.github.com>
2023-09-22 21:36:42 -0500

    Update oneLRT.R

--- R/oneLRT.R
+++ R/oneLRT.R
@@ -1,5 +1,14 @@
-oneLRT <- function(model1, model2) {
-  k1 <- model1$vc
-  if (model1$
-
+oneLRT <- function(mod0, mod1) {
+  k0 <- nrow(mod1$vc)
+  k1 <- nrow(mod0$vc)
+  if (k1 > k0) {
+    LRTstat <- -2 * (log(mod0$logl) - log(mod1$logl))
+    pval <- 1 - pchisq(LRTstat, (k1 - k0))
+  } else if (k0 > k1) {
+    LRTstat <- -2 * (log(mod1$logl) - log(mod0$logl))
+    pval <- 1 - pchisq(LRTstat, (k0 - k1))
+  } else {
+    stop("models must differ in their number of parameters")
+  }
+  list(LRTstat, pval)
 }
