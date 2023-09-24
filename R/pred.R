7ece754cbf864949afaad1169ce523475c1dd7e4
rtgodwin <42013175+rtgodwin@users.noreply.github.com>
2023-09-24 17:00:23 -0500

    Update pred.R

--- R/pred.R
+++ R/pred.R
@@ -24,21 +24,21 @@ pred <- function(model, data, maxpred) {
     th <- l / a
   }
   
-  if(class(model) == "truncmodel" & model1$dist == "Poisson") {
+  if(class(model) == "truncmodel" & model$dist == "Poisson") {
     #PP
     pred <- rep(0, maxpred)
     for(pp in 1:(maxpred)){
       pred[pp] <- sum((l ^ (pp)) / ((exp(l) - 1) * factorial(pp)))
     }
     pred
-  } else if(class(model) == "truncmodel" & model1$dist == "negbin") {
+  } else if(class(model) == "truncmodel" & model$dist == "negbin") {
     #ZTNB
     pred <- rep(0, maxpred)
     for(pp in 1:(maxpred)){
       pred[pp] <- sum((gamma(a + pp) / gamma(a) / gamma(pp + 1)) * ((1 / (1 + th)) ^ a) * ((th / (1 + th)) ^ pp) * (1 / (1 - (1 + th) ^ (-a))))
     }
     pred
-  } else if(class(model) == "oneinflmodel" & model1$dist == "Poisson") {
+  } else if(class(model) == "oneinflmodel" & model$dist == "Poisson") {
     #OIPP
     pred <- rep(0, maxpred)
     w <- -l * (exp(l) - l - 1) ^ -1 + (1 + l * (exp(l) - l - 1) ^ -1) * (1 + t) ^ -1
@@ -47,7 +47,7 @@ pred <- function(model, data, maxpred) {
       pred[pp] <- sum((1 - w) * (l ^ (pp)) / ((exp(l) - 1) * factorial(pp)))
     }
     pred
-  } else if(class(model) == "oneinflmodel" & model1$dist == "negbin") {
+  } else if(class(model) == "oneinflmodel" & model$dist == "negbin") {
     #OIZTNB
     pred <- rep(0, maxpred)
     P1 <- a * ((1 / (1 + th)) ^ a) * th / (1 + th - (1 + th) ^ (1 - a))
