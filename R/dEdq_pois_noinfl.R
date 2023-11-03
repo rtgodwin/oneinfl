9e4a14ad22cd7d8ec7c769ffe6c73a3029166b49
rtgodwin <42013175+rtgodwin@users.noreply.github.com>
2023-11-03 12:53:02 -0500

    Update dEdq_pois_noinfl.R

--- R/dEdq_pois_noinfl.R
+++ R/dEdq_pois_noinfl.R
@@ -1,15 +1,13 @@
 dEdq_pois_noinfl <- function(b, X, dummies) {
   l <- exp(X %*% b)
-  w <- 0
-  
-  dldq <- matrix(, nrow(X), (ncol(data) - 1))
-  colnames(dldq) <- colnames(data[-1])
+    
+  dldq <- matrix(, nrow(X), (ncol(X) - 1))
+  colnames(dldq) <- colnames(X[-1])
   dldq[, colnames(X)[-1]] <- l %*% b[-1]
   dldq[, -which(colnames(dldq) %in% colnames(X)[-1])] <- 0L
 
   # Do the math
-  dwdq <- 0
-  dEdq <- dwdq * as.vector((1 - (l * exp(l)) / (exp(l) - 1))) + dldq * as.vector((1 - w) * exp(l) * (exp(l) - l - 1) / (exp(l) - 1) ^ 2)
+  dEdq <- dldq * exp(l) * (exp(l) - l - 1) / (exp(l) - 1) ^ 2)
   
   # Calculate the "marginal" effect for dummies properly
   if(length(dummies) == 0) {return(dEdq)}
