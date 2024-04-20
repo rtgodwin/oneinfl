1d384342dfac5780ca461c640f5b2fd5c65b8c6b
rtgodwin <42013175+rtgodwin@users.noreply.github.com>
2024-04-20 13:48:47 -0500

    Update makeXZy.R

--- R/makeXZy.R
+++ R/makeXZy.R
@@ -4,27 +4,39 @@ makeXZy <- function(formula, data) {
     mf <- model.frame(formula = formula, data = data)
     y <- model.response(mf, "numeric")
     list(X = X, Z = Z, y = y)
-  } else {
-    if (length(formula[[3]]) > 1 && identical(formula[[3]][[1]], as.name("|"))) {
+  } else if (formula[[3]][[3]] == 1) {
     ff <- formula
     formula[[3]][1] <- call("+")
     ffc <- . ~ .
-    ffz <- ~.
     ffc[[2]] <- ff[[2]]
     ffc[[3]] <- ff[[3]][[2]]
-    ffz[[3]] <- ff[[3]][[3]]
-    ffz[[2]] <- NULL
+    mf <- model.frame(formula = formula, data = data)
+    mtX <- terms(ffc, data = data)
+    X <- model.matrix(mtX, mf)
+    Z <- model.matrix(~1, data.frame(intercept = rep(1, nrow(data))))
+    y <- model.response(mf, "numeric")
+    list(X = X, Z = Z, y = y)
   } else {
-    ffz <- ffc <- ff <- formula
-    ffz[[2]] <- NULL
-  }
-  mf <- model.frame(formula = formula, data = data)
-  mtX <- terms(ffc, data = data)
-  X <- model.matrix(mtX, mf)
-  mtZ <- terms(ffz, data = data)
-  mtZ <- terms(update(mtZ, ~.), data = data)
-  Z <- model.matrix(mtZ, mf)
-  y <- model.response(mf, "numeric")
-  list(X = X, Z = Z, y = y)
+    if (length(formula[[3]]) > 1 && identical(formula[[3]][[1]], as.name("|"))) {
+      ff <- formula
+      formula[[3]][1] <- call("+")
+      ffc <- . ~ .
+      ffz <- ~.
+      ffc[[2]] <- ff[[2]]
+      ffc[[3]] <- ff[[3]][[2]]
+      ffz[[3]] <- ff[[3]][[3]]
+      ffz[[2]] <- NULL
+    } else {
+      ffz <- ffc <- ff <- formula
+      ffz[[2]] <- NULL
+    }
+    mf <- model.frame(formula = formula, data = data)
+    mtX <- terms(ffc, data = data)
+    X <- model.matrix(mtX, mf)
+    mtZ <- terms(ffz, data = data)
+    mtZ <- terms(update(mtZ, ~.), data = data)
+    Z <- model.matrix(mtZ, mf)
+    y <- model.response(mf, "numeric")
+    list(X = X, Z = Z, y = y)
   }
 }
