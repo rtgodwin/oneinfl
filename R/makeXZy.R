3ec7422a5119bf7f49b5368a03412a26db64c156
rtgodwin <42013175+rtgodwin@users.noreply.github.com>
2024-04-25 22:12:05 -0500

    Update makeXZy.R

--- R/makeXZy.R
+++ R/makeXZy.R
@@ -4,32 +4,36 @@ makeXZy <- function(formula, data) {
     mf <- model.frame(formula = formula, data = data)
     y <- model.response(mf, "numeric")
     list(X = X, Z = Z, y = y)
-  } else if (formula[[3]][[3]] == 1) {
-    ff <- formula
-    formula[[3]][1] <- call("+")
-    ffc <- . ~ .
-    ffc[[2]] <- ff[[2]]
-    ffc[[3]] <- ff[[3]][[2]]
-    mf <- model.frame(formula = formula, data = data)
-    mtX <- terms(ffc, data = data)
-    X <- model.matrix(mtX, mf)
-    Z <- model.matrix(~1, data.frame(intercept = rep(1, nrow(data))))
-    y <- model.response(mf, "numeric")
-    list(X = X, Z = Z, y = y)
-  } else {
-    if (length(formula[[3]]) > 1 && identical(formula[[3]][[1]], as.name("|"))) {
+  }
+  testerror <- try(formula[[3]][[3]] == 1, silent = TRUE)
+  if (!inherits(testerror, "try-error")) {
+    if (formula[[3]][[3]] == 1) {
       ff <- formula
       formula[[3]][1] <- call("+")
       ffc <- . ~ .
-      ffz <- ~.
       ffc[[2]] <- ff[[2]]
       ffc[[3]] <- ff[[3]][[2]]
-      ffz[[3]] <- ff[[3]][[3]]
-      ffz[[2]] <- NULL
-    } else {
+      mf <- model.frame(formula = formula, data = data)
+      mtX <- terms(ffc, data = data)
+      X <- model.matrix(mtX, mf)
+      Z <- model.matrix(~1, data.frame(intercept = rep(1, nrow(data))))
+      y <- model.response(mf, "numeric")
+      list(X = X, Z = Z, y = y)
+      }
+  } 
+  if (length(formula[[3]]) > 1 && identical(formula[[3]][[1]], as.name("|"))) {
+    ff <- formula
+    formula[[3]][1] <- call("+")
+    ffc <- . ~ .
+    ffz <- ~.
+    ffc[[2]] <- ff[[2]]
+    ffc[[3]] <- ff[[3]][[2]]
+    ffz[[3]] <- ff[[3]][[3]]
+    ffz[[2]] <- NULL
+  } else {
       ffz <- ffc <- ff <- formula
       ffz[[2]] <- NULL
-    }
+  }
     mf <- model.frame(formula = formula, data = data)
     mtX <- terms(ffc, data = data)
     X <- model.matrix(mtX, mf)
@@ -38,5 +42,5 @@ makeXZy <- function(formula, data) {
     Z <- model.matrix(mtZ, mf)
     y <- model.response(mf, "numeric")
     list(X = X, Z = Z, y = y)
-  }
+  
 }
